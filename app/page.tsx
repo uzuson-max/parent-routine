938d66024cc98b4bddf0a56974acc4fbbf56febd
uzuson-max <uzuson@gmail.com>
2026-08-10 03:54:16 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '40px', fontFamily: 'sans-serif' }}>
-      <h1>부모님 루틴 관리 서비스</h1>
-      <p>정상적으로 배포가 완료되었습니다!</p>
-    </main>
-  );
-}
